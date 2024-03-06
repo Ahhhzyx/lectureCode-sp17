de66b3d4a7dd1b49f731a16310847f2a2b3225b0
Ahhhzyx <752165966@qq.com>
2024-03-06 10:15:38 +0800

    Update ExtraIntListPractice.java

--- lists1/exercises/ExtraIntListPractice.java
+++ lists1/exercises/ExtraIntListPractice.java
@@ -4,7 +4,11 @@ public class ExtraIntListPractice {
       * to change. */
     public static IntList incrList(IntList L, int x) {
         /* Your code here. */
-        return L;        
+        if(L == null){
+            return L;
+        }
+        IntList Q = new IntList(L.first + x, incrList(L.rest,x));
+        return Q;
     }
 
     /** Returns an IntList identical to L, but with
@@ -12,13 +16,18 @@ public class ExtraIntListPractice {
       * the 'new' keyword. */
     public static IntList dincrList(IntList L, int x) {
         /* Your code here. */
+        IntList dincr = L;
+        while(dincr != null){
+            dincr.first += x;
+            dincr = dincr.rest;
+        }
         return L;
     }
 
     public static void main(String[] args) {
         IntList L = new IntList(5, null);
-        L.tail = new IntList(7, null);
-        L.tail.tail = new IntList(9, null);
+        L.rest = new IntList(7, null);
+        L.rest.rest = new IntList(9, null);
 
         System.out.println(L.size());
         System.out.println(L.iterativeSize());
@@ -26,8 +35,19 @@ public class ExtraIntListPractice {
         // Test your answers by uncommenting. Or copy and paste the
         // code for incrList and dincrList into IntList.java and
         // run it in the visualizer.
-        // System.out.println(L.get(1));
-        // System.out.println(incrList(L, 3));
-        // System.out.println(dincrList(L, 3));        
+        System.out.println(L.get(1));
+        //System.out.println(incrList(L, 3));
+        incrList(L,3);
+        while(L!=null){
+            System.out.println(L.first);
+            L = L.rest;
+        }
+
+        System.out.println("------------------------------");
+        dincrList(L, 3);
+        while(L!=null){
+            System.out.println(L.first);
+            L = L.rest;
+        }
     }
 }
